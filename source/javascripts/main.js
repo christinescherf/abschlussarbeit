2fda7e5ca045dfc4ebd7fc1a445c86c70b7360f4
Christian Kesselheim <christian.kesselheim@kreios.lu>
2013-02-05 23:30:46 +0100

    Funktion zum einbetten von Bildern ("Fancybox") hinzugefügt

---
+++ source/javascripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".thumbnail").fancybox({
+        openEffect	: 'elastic',
+        closeEffect	: 'elastic',
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+});
